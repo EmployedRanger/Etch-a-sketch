3243e82765a944cbc9dc10eb74b440d7a5126d8e
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-02-18 16:53:12 -0700

    Completed Etch-a-sketch game

---
+++ README.md
@@ -0,0 +1,7 @@
+Complete Etch-a-sketch.
+
+Can choose a color, have a random color chosen for you,
+draw different colors every box, erase your boxes,
+choose the grid size, and reset the entire board. 
+
+Done by: Employed Ranger
--- javascript.js
+++ javascript.js
@@ -1,72 +1,125 @@
 // Etch-a-sketch
 
-const colorButton = document.getElementsByClassName('.color .button');
-const rainbowButton = document.getElementsByClassName('.rainbow .button');
-const shadeButton = document.getElementsByClassName('.shade .button');
-const lightenButton = document.getElementsByClassName('.shade .button');
-const eraserButton = document.getElementsByClassName('.eraser .button');
-const resetButton = document.getElementsByClassName('.reset .button');
+const colorButton = document.querySelector('.color .button');
+const rainbowButton = document.querySelector('.rainbow .button');
+const randomButton = document.querySelector('.random .button ');
+const eraserButton = document.querySelector('.eraser .button');
+const resetButton = document.querySelector('.reset .button');
+const gameBoard = document.querySelector('.game-board');
 
+const colorPickerBtn = document.querySelector('.color-picker-btn');
+const colorPicker = document.querySelector('.color-picker');
+const activeColor = document.querySelector('.active-color');
+const value = document.querySelector("#value");
+const input= document.querySelector("#slider");
+const boxes = document.querySelectorAll('div.box');
 
-const dimensions = document.getElementsByClassName('.dimensions');
-const gameBoard = document.getElementsByClassName('.game-board');
-
+let selectedColor = '#000';
+let canDraw = false;
 let quantity = 40;
+
+
+colorPickerBtn.addEventListener('click', () => {
+    colorPicker.click();
+});
+
+
+// User picks color
+colorPicker.addEventListener('input', (event) => {
+    selectedColor = event.target.value;
+    colorPickerBtn.style.backgroundColor = selectedColor;
+    activeColor.style.backgroundColor = selectedColor;
+    canDraw = true;
+});
+
 
 const boxStyles = {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
+    boxSizing: 'border-box',
     margin: '1px',
     backgroundColor: "black",
-    
 }
 
+
 // Player moves slider, values change and calls generateBoxes
-const value = document.querySelector("#value");
-const input= document.querySelector("#slider");
 value.textContent = input.value + " x " + input.value;
 input.addEventListener("input", (event) => {
     value.textContent = event.target.value + " x " + event.target.value;
     quantity = parseInt(event.target.value);   
-    console.log("boxes" + quantity);
+    console.log("boxes: " + quantity);
     generateBoxes(quantity);
-
 });
 
 
-
-
+// Creates boxes based on how many user chose
 function generateBoxes(quantity) {
-    quantity *= quantity;
-    console.log(quantity);
+    gameBoard.innerHTML = '';
+    rainbowTog = false;
 
+    gameBoard.style.gridTemplateColumns = `repeat(${quantity}, 1fr`;
+    gameBoard.style.gridTemplateRows = `repeat(${quantity}, 1fr`;
+
+    for (let i = 0; i < quantity ** 2; i++) {
+        const box = document.createElement('div');
+        box.className = 'box';
+        box.style.backgroundColor = activeColor.style.backgroundColor || '#000';
+
+        Object.assign(box.style, boxStyles);
+        gameBoard.appendChild(box);
+
+        box.addEventListener('mouseover', () => {
+            if(canDraw) {
+                if (rainbowTog) {
+                    box.style.backgroundColor = randomColor();
+                    
+                } else {
+                    box.style.backgroundColor = selectedColor || 
+                    activeColor.style.backgroundColor;
+                }
+            }
+        });
+    }
 }
 
 
+rainbowButton.addEventListener('click', () => {
+    rainbowTog = true;
+})
+
+function randomInteger(max) {
+    return Math.floor(Math.random()*(max + 1));
+}
+
+function randomColor () {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+randomButton.addEventListener('click', () => {
+    rainbowTog = false;
+    randColor = randomColor();
+    activeColor.style.backgroundColor = randColor;
+    selectedColor = randColor;
+    activeColor.style.backgroundColor = selectedColor;
+    canDraw = true;
+});
+
+resetButton.addEventListener('click', () => {
+    generateBoxes(quantity);
+    rainbowTog = false;
+});
+
+eraserButton.addEventListener('click', () => {
+    rainbowTog = false;
+    selectedColor = '#000';
+    activeColor.style.backgroundColor = selectedColor;
+    console.log('here');
+    console.log(selectedColor);
+});
 
 
-
-// function generate boxes
-    // Takes number as parameter
-    // # of boxes show up on screen
-
-
-
-
-// Each button listens for event
-    // returns color change
-
-// rainbow waits for click
-    // runs rainbow function
-
-// rainbow function 
-    // 
-
-
-// Reset button listens for click
-    // runs reset function
-
-// reset function 
-    // for loop that goes over each box and makes it grey
-
+generateBoxes(40);
